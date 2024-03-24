3e4451bc62a5ff1765f2a77652d0b8ab8c84aa31
ThabataSantinho <thabata_santinho@hotmail.com>
2024-03-24 16:32:08 -0300

    Exercicios 1,2,3

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -1,16 +1,17 @@
-import ex2.JogoAdivinhacao;/* /ex6 na lista: Implemente uma classe que modele um jogo de adivinhacao de
+import ex2.JogoAdivinhacao;
+
+/* ex6 na lista: Implemente uma classe que modele um jogo de adivinhacao de
 n´umeros de 0 a 99. Atributos: um numero inteiro sorteado. Metodos: sortear(),
-advinhar(). OBS: O objeto para gerar n´umero aleat´orios no Java ´e o Random;
+advinhar(). OBS: O objeto para gerar n´umero aleatorios no Java e o Random;
 voce deve instanci´a-lo e chamar seu m´etodo nextInt() que deve possuir um
         argumento inteiro, no caso aqui, 100.*/
 public static void main(String[] args) {
-    // Criando uma instância do jogo
+
     JogoAdivinhacao jogo = new JogoAdivinhacao();
 
-    // Sorteando um número
     jogo.sortear();
 
-    // Tentando adivinhar o número sorteado
-    int palpite = 50; // Exemplo de palpite
+    int palpite = 50;
     jogo.adivinhar(palpite);
 }
+
