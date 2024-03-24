95ff39e2575bc7e510aee50328ab950112c86674
ThabataSantinho <thabata_santinho@hotmail.com>
2024-03-24 16:52:37 -0300

    Exercicios 1,2,3

--- src/ex4/Main.java
+++ src/ex4/Main.java
@@ -1,4 +1,11 @@
 package ex4;
+/*Implemente a classe Cliente que possua os atributos nome,
+saldo, e limite. Esta deve possuir tambem os m´etodos sacar(), depositar(),
+checarSaldo(), e obterNome(). Sabe-se que so e possıvel sacar se o saldo+limite
+forem superiores a quantia. Os metodos sacar() e depositar() necessitam de
+parametros. O metodo checarSaldo deve retornar o valor saldo+limite. O
+metodo obterNome() deve retornar o nome do Cliente.*/
+
 public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("João", 1000, 500);
